a99136e989fe8e0d6b3fbccd4a93fdb50431304e
ZArkadia <wiselyzzz@gmail.com>
2025-01-08 04:58:20 +0700

    Added SMT VV templates from DeathChaos

---
+++ SMTV Note Help by DeathChaos.v
@@ -0,0 +1,33 @@
+SMTV-010Templates
+010 Templates for Shin Megami Tensei binary data tables
+
+010 Editor version 12 or newer needed.
+
+Check the individual .h files inside formats folder to see what files are currently supported and where to find them in your game dump.
+
+Currently Supported Files
+Project\Content\Blueprints\Gamedata\BinTable\Devil\NKMBaseTable.uexp
+
+This file contains all the basic stats data for demons, both allies and enemies, including (but not limited to):
+
+Stats (atk/vit/mag/etc)
+Demon Race
+Base Level
+Base Affinities
+Base Skill Affinites
+etc
+Project\Content\Blueprints\Gamedata\BinTable\Battle\Skill\SkillData.uexp
+
+As the name implies, this file contains data for skills.
+
+Project\Content\Blueprints\Gamedata\BinTable\Map\EncountData.uexp
+
+This file contains data pertaining to encounters, contains data for both the overworld demons shown and what encounter each demon will result in.
+
+Project\Content\Blueprints\Gamedata\BinTable\Unite\UniteCombineTable.uexp
+
+This file contains data for possible Demon Fusions and their result.
+
+Project\Content\Blueprints\Gamedata\BinTable\Common\PlayerGrow.uexp
+
+This file, as the name implies, contains data pertaining to the player character's stat gains per level.
